b2e046f5d3a9f4ad05da32cb5d9e87a9476e0b1d
abe <abraham.bloom.21@gmail.com>
2023-11-02 20:58:12 -0700

    Initial Commit

---
+++ deadcode/deadcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  deadcodeApp.swift
+//  deadcode
+//
+//  Created by Abraham Bloom on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct deadcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
